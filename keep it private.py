4667c5da0cc2d8d5d746d3578142a5c2758db170
Bdsujanctg <165937117+Bdsujanctg@users.noreply.github.com>
2025-05-09 10:47:21 +0600

    Add files via upload

---
+++ keep it private.py
@@ -0,0 +1,8 @@
+class Creation:
+    __privatevar=27
+    def __privatemeth(self):
+        print("I am inside class myClass")
+    def hello():
+       print(Creation.__privatevar)
+obj=Creation()
+obj.hello()
